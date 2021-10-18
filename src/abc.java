ffa9728778dcb921f86e5f441290d423de432e91
ccc <1045070684@qq.com>
2021-10-18 16:40:37 +0800

    ccc

---
+++ src/abc.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * @author com.zzy
+ * @date 2021/10/15-17:02
+ */
+public class abc {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String[] s1 = s.split(" ");
+        for (int i=s1.length-1;i>=0;i--) {
+            System.out.print(s1[i]+" ");
+        }
+    }
+}
